16bc3179ad2d9e681aa790f35235d4ea2a0f2529
Obi William <williamobi818@gmail.com>
2024-01-05 17:20:34 +0000

    kyp

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -6,7 +6,7 @@ function handleProfileSignup() {
       console.log(values[0].body, values[1].firstName, values[1].lastName);
     })
     .catch(() => {
-      console.log('Signup system offline');
+      console.error('Signup system offline');
     });
 }
 
