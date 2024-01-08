8dcc041f90d4b77282e3736b8631e8c93ae909d5
Obi William <williamobi818@gmail.com>
2024-01-08 20:03:12 +0000

    ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+export default function getListStudentIds(students) {
+  // Check if the argument is an array
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  // Use map to extract the ids from the array of objects
+  return students.map((student) => student.id);
+}
