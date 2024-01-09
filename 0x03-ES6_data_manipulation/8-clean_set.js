1e4634db6aef86529908954ffe1868d51a2c5e4e
Obi William <williamobi818@gmail.com>
2024-01-09 09:43:18 +0000

    rest of the tasks

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') return '';
+
+  let result = '';
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      if (result !== '') result += '-';
+      result += item.slice(startString.length);
+    }
+  }
+
+  return result;
+}
