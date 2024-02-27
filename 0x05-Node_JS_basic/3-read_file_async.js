94db62aaf8dd7a07ed27769385ff60d69648fc5d
Obi William <williamobi818@gmail.com>
2024-02-27 23:20:38 +0100

    nodekpi

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,26 +4,26 @@ module.exports = function countStudents(path) {
   return fs.readFile(path, 'utf8')
     .then((data) => {
       // Remove empty lines
-      const lines = data.split('\n').filter(line => line);
+      const lines = data.split('\n').filter((line) => line);
 
       // Remove the header line
       lines.shift();
 
-      const students = lines.map(line => ({
+      const students = lines.map((line) => ({
         firstname: line.split(',')[0],
         field: line.split(',')[3],
       }));
 
       console.log(`Number of students: ${students.length}`);
 
-      const fields = [...new Set(students.map(student => student.field))];
+      const fields = [...new Set(students.map((student) => student.field))];
 
       fields.forEach((field) => {
-        const studentsInField = students.filter(student => student.field === field);
-        console.log(`Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.map(s => s.firstname).join(', ')}`);
+        const studentsInField = students.filter((student) => student.field === field);
+        console.log(`Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.map((s) => s.firstname).join(', ')}`);
       });
     })
     .catch(() => {
       throw new Error('Cannot load the database');
     });
-}
+};
