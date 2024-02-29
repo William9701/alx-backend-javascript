0b3a5a509bae2b2096eddbc73eaa54fb49165eb0
Obi William <williamobi818@gmail.com>
2024-02-29 21:36:17 +0100

    task 4-ud

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,11 +3,28 @@ const sendPaymentRequestToApi = require("./3-payment");
 const Utils = require("./utils");
 
 describe("sendPaymentRequestToApi", function () {
-  it("should use the Utils function", function () {
-    const spy = sinon.spy(Utils, "calculateNumber");
+  let consoleLogSpy;
+
+  beforeEach(function () {
+    consoleLogSpy = sinon.spy(console, "log");
+
+    sinon.stub(Utils, "calculateNumber").returns(10);
+  });
+
+  afterEach(function () {
+    Utils.calculateNumber.restore();
+    consoleLogSpy.restore();
+  });
+
+  it("should use the Utils function with the correct arguments", function () {
     sendPaymentRequestToApi(100, 20);
 
-    sinon.assert.calledWith(spy, "SUM", 100, 20);
-    spy.restore();
+    sinon.assert.calledWith(Utils.calculateNumber, "SUM", 100, 20);
+  });
+
+  it("should log the correct message", function () {
+    sendPaymentRequestToApi(100, 20);
+
+    sinon.assert.calledWith(consoleLogSpy, "The total is: 10");
   });
 });
--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,26 +3,18 @@ const sendPaymentRequestToApi = require("./3-payment");
 const Utils = require("./utils");
 
 describe("sendPaymentRequestToApi", function () {
-  it("should use the Utils function and log the correct message", function () {
-    // Stub Utils.calculateNumber to always return 10
-    const calculateNumberStub = sinon
-      .stub(Utils, "calculateNumber")
-      .returns(10);
+  it("should use the Utils function", function () {
+    const stub = sinon.stub(Utils, "calculateNumber");
+    stub.withArgs("SUM", 100, 20).returns(10);
 
-    // Spy on console.log
-    const consoleLogSpy = sinon.spy(console, "log");
+    const consoleSpy = sinon.spy(console, "log");
 
-    // Call the function
     sendPaymentRequestToApi(100, 20);
 
-    // Verify that the stub is being called with the correct arguments
-    sinon.assert.calledWith(calculateNumberStub, "SUM", 100, 20);
+    sinon.assert.calledWith(stub, "SUM", 100, 20);
+    sinon.assert.calledWith(consoleSpy, "The total is: 10");
 
-    // Verify that console.log is logging the correct message
-    sinon.assert.calledWith(consoleLogSpy, "The total is: 10");
-
-    // Restore the stub and the spy
-    calculateNumberStub.restore();
-    consoleLogSpy.restore();
+    stub.restore();
+    consoleSpy.restore();
   });
 });
