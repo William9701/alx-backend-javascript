dc55060927c399621b5b738e91bde3f9e610f613
Obi William <williamobi818@gmail.com>
2024-02-29 18:00:16 +0100

    task 3 updatesd

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,12 +1,20 @@
-const chai = require("chai");
-const { expect } = chai;
-const calculateNumber = require("./1-calcul");
+const { expect } = require("chai");
+const calculateNumber = require("./2-calcul_chai.js");
 
-describe("calculateNumber", function () {
-  it("should return the calc of rounded a and b", function () {
+describe("calculateNumber function", () => {
+  it("should return the sum of two rounded numbers", () => {
     expect(calculateNumber("SUM", 1.4, 4.5)).to.equal(6);
+  });
+
+  it("should return the subtraction of two rounded numbers", () => {
     expect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(-4);
+  });
+
+  it("should return the division of two rounded numbers", () => {
     expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2);
+  });
+
+  it('should return "Error" for division when roundedB is 0', () => {
     expect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error");
   });
 });
