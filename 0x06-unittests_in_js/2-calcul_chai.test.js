dd074fbee7fb0fcce988d2f4d6fe851a95719f24
Obi William <williamobi818@gmail.com>
2024-02-29 17:31:10 +0100

    task 3 update

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -3,9 +3,9 @@ const calculateNumber = require("./1-calcul");
 
 describe("calculateNumber", function () {
   it("should return the calc of rounded a and b", function () {
-    expect(calculateNumber("SUM", 1.4, 2.6)).to.equal(4);
-    expect(calculateNumber("SUBTRACT", 2.5, 1.4)).to.equal(2);
-    expect(calculateNumber("DIVIDE", 2.5, 1.4)).to.equal(3);
-    expect(calculateNumber("DIVIDE", 2.5, 0)).to.equal("Error");
+    expect(calculateNumber("SUM", 1.4, 4.5)).to.equal(6);
+    expect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(-4);
+    expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2);
+    expect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error");
   });
 });
